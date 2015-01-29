a83c7b44b8ad0b4938e11a81c614ebe3a4b7b967
zhangtreefish <zhangtreefish@users.noreply.github.com>
2015-01-29 14:38:24 -0600

    Try again to test credential helper

--- lesson_3_reflections.tcl
+++ lesson_3_reflections.tcl
@@ -8,3 +8,4 @@ it/'
 can not figure out why. One instrutor offered that "I've ran into this before, your password or username is inconsistent with your Github login." I reentered the username and password thousands of time (may be dozens of times) and of course it did not work.
 Finally
 "I thought of setting the @users.noreply.github.com email address as the public address in gitHub (previously my personal email was set as the public email), as well as the user email at git [using config --global command]. Then the push origin master worked without getting stuck at the password. How I wish the error message were 'git user email not recognized as the public email at gitHub!' "
+--There will be next one challenge around the corner! I hope each will not be that time-consuming!
